194251c650c36fe06b5630e8b1c2ac50ae2b0e58
shimousaminato <shimousaminato@gmail.com>
2025-08-31 12:59:11 +0900

    Create project1.js

---
+++ javascript/project1.js
@@ -0,0 +1,7 @@
+function submit() {
+const comment = document.getElementById('comment')
+const test = document.getElementById('test')
+
+const content = comment.value;
+div.textContent = content;
+}
