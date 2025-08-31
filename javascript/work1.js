063bac1fdb5bfd82a67689936131c21829346096
shimousaminato <shimousaminato@gmail.com>
2025-08-31 13:18:55 +0900

    Update work1.js

--- javascript/work1.js
+++ javascript/work1.js
@@ -1,5 +1,5 @@
 function posting() {
-  const discordWebHookURL = "https://discord.com/api/webhooks/000000/XXXXX";
+  const discordWebHookURL = "https://discord.com/api/webhooks/1389048593352228908/2XX9ryH4FtQx_Of4LRKPQGkfZtxO3LY2oqnvM0JiM4CDYQIE01oRnrPOGr1hJtCDvGJm";
 
   const comment = document.getElementById('comment')
   const message = {
