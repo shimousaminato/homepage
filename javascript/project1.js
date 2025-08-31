a97b20aef56a5878bf59eb52b91b4ebb77216538
shimousaminato <shimousaminato@gmail.com>
2025-08-31 13:04:26 +0900

    Update project1.js

--- javascript/project1.js
+++ javascript/project1.js
@@ -3,5 +3,5 @@ const comment = document.getElementById('comment')
 const test = document.getElementById('test')
 
 const content = comment.value;
-div.textContent = content;
+test.textContent = content;
 }
