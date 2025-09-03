a72a3b554693268aca5fc5c73347f7ff72afe2c6
shimousaminato <shimousaminato@gmail.com>
2025-09-03 15:32:52 +0900

    Update work2.js

--- javascript/work2.js
+++ javascript/work2.js
@@ -11,6 +11,33 @@ function posting() {
  else if (optionId === "B") {
   discordWebHookURL = "https://discord.com/api/webhooks/1412678875753873448/z_IuLTcmedIrqsev0Tokl_0FXo31oklcxvmptORK4NIsrIrIe9xBYU8T0kRzOe7losZa";
  }
+ else if (optionId === "C") {
+  discordWebHookURL = "https://discord.com/api/webhooks/1412684147234836512/_JF8rUVWMbZrxzr44Zt5fcJI4c2hqtqcH5I9T6cqBcmZufGzZh7Gt4id_YCes8_4K74e";
+ }
+ else if (optionId === "D") {
+  discordWebHookURL = ""
+ }
+ else if (optionId === "E") {
+  discordWebHookURL = ""
+ }
+  else if (optionId === "F") {
+  discordWebHookURL = ""
+ }
+  else if (optionId === "G") {
+  discordWebHookURL = ""
+ }
+  else if (optionId === "H") {
+  discordWebHookURL = ""
+ }
+  else if (optionId === "I") {
+  discordWebHookURL = ""
+ }
+  else if (optionId === "J") {
+  discordWebHookURL = ""
+ }
+  else if (optionId === "K") {
+  discordWebHookURL = ""
+ }
  else{
   discordWebHookURL = "https://discord.com/api/webhooks/1389048593352228908/2XX9ryH4FtQx_Of4LRKPQGkfZtxO3LY2oqnvM0JiM4CDYQIE01oRnrPOGr1hJtCDvGJm";
  }
