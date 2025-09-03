cb1fbaa7c30fc233da4a4b35c27ec01dce770b61
shimousaminato <shimousaminato@gmail.com>
2025-09-03 14:50:14 +0900

    Create work2.js

---
+++ javascrip/work2.js
@@ -0,0 +1,6 @@
+function submit() {
+ const selectElement = document.getElementById('testselect');
+ const selectedOption = selectElement.options[selectElement.selectedIndex];
+ const optionId = selectedOption.id;
+ test.textContent = optionId;
+}
