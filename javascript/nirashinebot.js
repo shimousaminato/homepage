534e8824404c875649765e8e6614b56d2147b4e5
shimousaminato <shimousaminato@gmail.com>
2025-09-04 14:35:02 +0900

    Update nirashinebot.js

--- javascript/nirashinebot.js
+++ javascript/nirashinebot.js
@@ -1,5 +1,5 @@
 function posting() {
-  const discordWebHookURL = "https://discord.com/api/webhooks/1412613762569207899/_6I0Pg5O7EqgAP-oXJqw7diMphEo-CzHdlY7fxUQveTDm1kP7j1jjc1hIXVlZouW46zT";
+  const discordWebHookURL = "https://discord.com/api/webhooks/1389048593352228908/2XX9ryH4FtQx_Of4LRKPQGkfZtxO3LY2oqnvM0JiM4CDYQIE01oRnrPOGr1hJtCDvGJm";
 
 
   // fetch APIを使ってDiscordにメッセージを送信
