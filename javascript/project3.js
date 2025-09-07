0160cc8fe5dc9352b15efd14d420c98bd061adf4
shimousaminato <shimousaminato@gmail.com>
2025-09-08 08:53:16 +0900

    Create project3.js

---
+++ javascript/project3.js
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+# 取得したいURL
+url = "https://ku-tetsu.net/"
+
+# ウェブページの内容を取得
+response = requests.get(url)
+html_content = response.content
+
+# BeautifulSoupオブジェクトを作成
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# idが「dIndexListDataSeq0」のdiv要素を検索
+target_div = soup.find('div', id='dIndexListDataSeq0')
+
+console.log(target_div)
