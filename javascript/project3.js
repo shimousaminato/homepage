c482855a2fe3721de0299e57ef911be511060e6e
shimousaminato <shimousaminato@gmail.com>
2025-09-08 08:54:05 +0900

    Update project3.js

--- javascript/project3.js
+++ javascript/project3.js
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+function search() {
 # 取得したいURL
 url = "https://ku-tetsu.net/"
 
@@ -15,3 +16,4 @@ soup = BeautifulSoup(html_content, 'html.parser')
 target_div = soup.find('div', id='dIndexListDataSeq0')
 
 console.log(target_div)
+}
